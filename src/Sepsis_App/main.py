16490e7b69b9f37e4f170c9dbe0b57882fab323d
Jacob Jaroya <jaroyajo@gmail.com>
2023-08-20 15:32:26 +0200

    extra changes

--- src/Sepsis_App/main.py
+++ src/Sepsis_App/main.py
@@ -151,4 +151,4 @@ async def sepsis_classification(sepsis: Sepsis):
 
 # Run the FastAPI application using uvicorn
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload = True)
+    uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
